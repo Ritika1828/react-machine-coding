128c2c0aeabed4622f114e979cad1a1638600893
ritika <ritika.juneja@byjus.com>
2024-05-08 12:08:20 +0530

    added progress bar

--- src/ProgressBar/index.js
+++ src/ProgressBar/index.js
@@ -1,12 +1,68 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-function ProgressBar(props) {
+const ProgressBarComp = ({ onComplete, isComplete }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (!isComplete) {
+      interval = setInterval(() => {
+        setProgress((prevProgress) => {
+          if (prevProgress < 100) {
+            return prevProgress + 20;
+          } else {
+            clearInterval(interval);
+            onComplete();
+            return 0;
+          }
+        });
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isComplete, onComplete]);
+
   return (
-    <div>ProgressBar</div>
-  )
-}
+    <div
+      style={{
+        border: "1px solid #ccc",
+        width: "100%",
+        marginTop: "10px",
+      }}
+    >
+      <div
+        style={{
+          width: `${progress}%`,
+          height: "20px",
+          backgroundColor: "#4caf50",
+        }}
+      ></div>
+    </div>
+  );
+};
 
-ProgressBar.propTypes = {}
+const ProgressBar = () => {
+  const [bars, setBars] = useState([]);
 
-export default ProgressBar
+  const handleCreate = () => {
+    setBars((prevBars) => [...prevBars, Date.now()]);
+  };
+
+  return (
+    <div>
+      <button onClick={handleCreate}>Create</button>
+      {bars.map((timestamp, index) => (
+        <ProgressBarComp
+          key={timestamp}
+          onComplete={() => setBars((prevBars) => prevBars.slice(1))}
+          isComplete={index !== 0}
+        />
+      ))}
+    </div>
+  );
+};
+
+ProgressBar.propTypes = {};
+
+export default ProgressBar;
